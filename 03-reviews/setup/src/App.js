ea8ffa55cf61b9da1b0cf587ba2fc18e30667b79
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-05-03 18:51:05 +0200

    reviews

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -2,9 +2,15 @@ import React from 'react'
 import Review from './Review'
 function App() {
   return (
-    <section>
-      <Review />
-    </section>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   )
 }
 
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,22 +5,47 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 const Review = () => {
   const [index, setIndex] = useState(0)
   const { id, name, job, image, text } = people(index)
+  const checkIndex = () => {
+    if (index > people.length - 1) {
+      return 0
+    }
+    if (index < 0) {
+      return people.length - 1
+    }
+  }
+  const moveRight = () => {
+    const person = index + 1
+    setIndex(checkIndex(person))
+  }
+  const moveLeft = () => {
+    const person = index - 1
+    setIndex(checkIndex(person))
+  }
+  const random = () => {
+    let person = Math.floor(Math.random(index * people.length))
+    setIndex(checkIndex(person))
+  }
   return (
-    <article>
-      <img src={image} alt={name} />
-      <FaQuoteRight className='' />
-      <h4>{name}</h4>
-      <h4>{job}</h4>
-      <p>{text}</p>
-      <div>
-        <span onClick={moveLeft}>
-          <FaChevronLeft />
-        </span>
-        <span onClick={moveRight}>
-          <FaChevronRight />
+    <article className='review'>
+      <div className='image-container'>
+        <img src={image} alt={name} />
+        <span className='quot-icon'>
+          <FaQuoteRight />
         </span>
       </div>
-      <button>Random quote</button>
+
+      <h4 className='author'>{name}</h4>
+      <h4 className='job'>{job}</h4>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={moveLeft}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={moveRight}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button onClick={random}>Random quote</button>
     </article>
   )
 }
